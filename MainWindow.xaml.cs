5cf44507e9940bab577d50f59fc23a28184469bf
rhagerman <rachel.hagerman@gmail.com>
2014-10-08 09:01:52 -0500

    Adding option to freeze selected row

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -14,6 +15,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+
+using Telerik.Windows.Controls;
 using Telerik.Windows.Controls.GridView;
 using Telerik.Windows.Data;
 using TriTechDemo.Data;
@@ -41,8 +44,6 @@ namespace TriTechDemo
             _dataChangeTimer.Interval = TimeSpan.FromMilliseconds(16);
             _dataChangeTimer.Tick += DataChangeTimerTick;
             _dataChangeTimer.Start();
-
-            
         }
 
         private void DataChangeTimerTick(object sender, EventArgs e)
@@ -50,7 +51,6 @@ namespace TriTechDemo
            MainViewModel vm =  (MainViewModel) spMain.DataContext;
           
            vm.DataSource.PerformRandomStatusChanges();
-           
         }
 
         void UnitViewSource_Filter(object sender, FilterEventArgs e)
@@ -85,6 +85,7 @@ namespace TriTechDemo
             }
         }
 
+        private Point positionInScrollViewer;
         private void GvUnitGrid_OnRowIsExpandedChanged(object sender, RowEventArgs e)
         {
             if (e.Row != null)
@@ -98,14 +99,58 @@ namespace TriTechDemo
                         _previousRow.IsExpanded = false;
                     }
                     _previousRow = row;
-
+                   
                 }
             }
         }
 
-        private void GvUnitGrid_OnFiltering(object sender, GridViewFilteringEventArgs e)
+        private void ScrollToSelected()
         {
-           
+            if (_previousRow != null)
+            {
+                var scroller = this.gvUnitGrid.ChildrenOfType<GridViewScrollViewer>().First();
+                try
+                {
+                    // see if the row has moved and move it back
+                    FrameworkElement element = _previousRow as FrameworkElement;
+                    var transform = element.TransformToVisual(scroller);
+                    var currentpositionInScrollViewer = transform.Transform(new Point(0, 0));
+                    Debug.WriteLine("Row found at Y offset {0}", currentpositionInScrollViewer.Y);
+                    if (Math.Abs(this.positionInScrollViewer.Y - currentpositionInScrollViewer.Y) > 10)
+                    {
+                        Debug.WriteLine("Original Scroller Y offset {0}", scroller.VerticalOffset);
+                        var diff = this.positionInScrollViewer.Y - currentpositionInScrollViewer.Y;
+                        Debug.WriteLine("Scroller diff {0}", diff);
+                        scroller.ScrollToVerticalOffset(scroller.VerticalOffset - diff);
+                        Debug.WriteLine("New Scroller Y offset {0}", scroller.VerticalOffset);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private void GvUnitGrid_OnLayoutUpdated(object sender, EventArgs e)
+        {
+            // layout has updated, check if we should move the row
+            if (cbFreezeRow.IsChecked.HasValue && cbFreezeRow.IsChecked.Value)
+            {
+                this.ScrollToSelected();
+            }
+            
+        }
+
+        private void CbFreezeRow_OnChecked(object sender, RoutedEventArgs e)
+        {
+            // track the location of this row
+            var scroller = this.gvUnitGrid.ChildrenOfType<GridViewScrollViewer>().First();
+            FrameworkElement element = _previousRow as FrameworkElement;
+            var transform = element.TransformToVisual(scroller);
+            positionInScrollViewer = transform.Transform(new Point(0, 0));
+            Debug.WriteLine("Row selected at Y offset {0}", positionInScrollViewer.Y);
+            Debug.WriteLine("Scroller Y offset {0}", scroller.VerticalOffset);
         }
     }
 }
